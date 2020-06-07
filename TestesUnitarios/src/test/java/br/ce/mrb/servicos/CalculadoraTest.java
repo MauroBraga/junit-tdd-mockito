f0a1a2e7a88d89a7da3a43443aba434a17da01b5
= <mrb0305@gmail.com>
2020-06-07 18:03:18 -0300

    TDD, parte 1

---
+++ TestesUnitarios/src/test/java/br/ce/mrb/servicos/CalculadoraTest.java
@@ -0,0 +1,67 @@
+package br.ce.mrb.servicos;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.ce.mrb.exceptions.NaoPodeDividirPorZeroException;
+
+public class CalculadoraTest {
+
+	private Calculadora calculadora ;
+	
+	@Before
+	public void setUp() {
+		calculadora= new Calculadora();
+	}
+	
+	@Test
+	public void deveSomarDoisValores() {
+		//cenario
+		int a = 5;
+		int b =3;
+		
+		//acao
+		int resultado = calculadora.somar(a , b);
+		
+		
+		//verificacao
+		Assert.assertEquals(8, resultado);
+	}
+	
+	@Test
+	public void deveSubtrairDoisValores() {
+		//cenario
+		int a = 8;
+		int b = 5;
+		
+		//ação
+		int resultado = calculadora.subtrair(a,b);
+		
+		//verficação
+		Assert.assertEquals(3, resultado);
+		
+	}
+	
+	@Test
+	public void deveDividirDoisValores() throws NaoPodeDividirPorZeroException {
+		//cenario 
+		int a = 6;
+		int b = 3;
+		
+		//acao
+		int resultado = calculadora.divide(a,b);
+				
+		//verificacao
+		Assert.assertEquals(resultado, 2);
+		
+	}
+	
+	@Test(expected=NaoPodeDividirPorZeroException.class)
+	public void deveLacarExcecaoAoDividirPorZero() throws NaoPodeDividirPorZeroException {
+		int a = 6;
+		int b = 0;
+		//acao
+		int resultado = calculadora.divide(a,b);
+	}
+}
